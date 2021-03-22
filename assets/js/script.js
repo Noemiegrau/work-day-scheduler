8ac827b50c98ec2980f9912b368517a45574e7a1
Noemiegrau <noemiegrau@outlook.fr>
2021-03-22 07:55:15 -0700

    updated comments in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,57 +1,42 @@
 $("document").ready(function() {
 
-    // Display the time
+    // Displays the time in the header of the page
     const today = moment();
     $("#currentDay").text(today.format('dddd' + ', ' + 'MMMM Do YYYY'));
 
-        // var textareaidEL = $(this).attr("id");
-
-        // function that verifies the time and gives background color
-        // function displayTimeColor() {
-        //let currentTime = moment().format('HH');
-        let currentTime = moment().hours()+3;
-        console.log(currentTime);
-
-        inputEl = $("textarea").each(function(){
-
-            var textEL = $(this).attr("id");
-            console.log(textEL);
-    
-            if ( textEL < currentTime) {
-                $(this).addClass("past");
-                // $('textarea').addClass("past");
-                // .removeClass("present")
-                // .removeClass("future");
-
-            } else if ( textEL == currentTime) {
-                $(this).addClass("present");
-                // $('textarea').addClass("present");
-                // .removeClass("past")
-                // .removeClass("future");
-
-            } else if ( textEL > currentTime) {
-                $(this).addClass("future");
-                // $('textarea').addClass("future");
-                // .removeClass("present")
-                // .removeClass("past");
-            } //else {
-            //     $('textarea').addClass("past")
-            //     .removeClass("present")
-            //     .removeClass("future");
-            // }
-
-            setInterval(function(){
-                inputEl
-            }, 1000);
-
-        });
-    
-    // Add event listeners to buttons to save inputs in localStorage
+    // Add event listeners to buttons to able saving data in localStorage
     $(".saveBtn").on('click', function() {
         var hour = $(this).parent().attr("id");
         var event = $(this).siblings(".description").val();
         localStorage.setItem(hour, event);
     });
+
+    // Sets variable value for current time 
+    let currentTime = moment().hours()+4;
+    // console.log(currentTime);
+
+    // Takes care of attributing the background colors according to the time of the day
+    inputEl = $("textarea").each(function(){
+
+        var textEL = $(this).attr("id");
+        console.log(textEL);
+    
+        if ( textEL < currentTime) {
+            $(this).addClass("past");
+
+        } else if ( textEL == currentTime) {
+            $(this).addClass("present");
+
+        } else if ( textEL > currentTime) {
+            $(this).addClass("future");
+        }
+
+        // Excecutes the function continuously and for each textarea line
+        setInterval(function(){
+            inputEl
+            }, 1000);
+
+        });
 
     // Data is retrieved from localStorage even after refreshing the page 
     $("#9am .description").val(localStorage.getItem("9am"));
@@ -63,7 +48,5 @@ $("document").ready(function() {
     $("#3pm .description").val(localStorage.getItem("3pm"));
     $("#4pm .description").val(localStorage.getItem("4pm"));
     $("#5pm .description").val(localStorage.getItem("5pm"));
-
-    // displayTimeColor();
 
 });
