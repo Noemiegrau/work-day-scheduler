4eed755ac1305748fc413cc30f169c4417d03d74
Noemiegrau <noemiegrau@outlook.fr>
2021-03-22 07:39:08 -0700

    updated background color bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,57 @@
-// Display the time
-const today = moment();
-$("#currentDay").text(today.format('dddd' + ', ' + 'MMMM Do YYYY'));
+$("document").ready(function() {
 
-$(document).ready(function () {
+    // Display the time
+    const today = moment();
+    $("#currentDay").text(today.format('dddd' + ', ' + 'MMMM Do YYYY'));
 
+        // var textareaidEL = $(this).attr("id");
+
+        // function that verifies the time and gives background color
+        // function displayTimeColor() {
+        //let currentTime = moment().format('HH');
+        let currentTime = moment().hours()+3;
+        console.log(currentTime);
+
+        inputEl = $("textarea").each(function(){
+
+            var textEL = $(this).attr("id");
+            console.log(textEL);
+    
+            if ( textEL < currentTime) {
+                $(this).addClass("past");
+                // $('textarea').addClass("past");
+                // .removeClass("present")
+                // .removeClass("future");
+
+            } else if ( textEL == currentTime) {
+                $(this).addClass("present");
+                // $('textarea').addClass("present");
+                // .removeClass("past")
+                // .removeClass("future");
+
+            } else if ( textEL > currentTime) {
+                $(this).addClass("future");
+                // $('textarea').addClass("future");
+                // .removeClass("present")
+                // .removeClass("past");
+            } //else {
+            //     $('textarea').addClass("past")
+            //     .removeClass("present")
+            //     .removeClass("future");
+            // }
+
+            setInterval(function(){
+                inputEl
+            }, 1000);
+
+        });
+    
     // Add event listeners to buttons to save inputs in localStorage
     $(".saveBtn").on('click', function() {
         var hour = $(this).parent().attr("id");
         var event = $(this).siblings(".description").val();
         localStorage.setItem(hour, event);
     });
-
-    // function that verifies the time and gives background color
-    function displayTimeColor() {
-        var currentTime = moment().format('HH');
-
-        console.log(currentTime);
-        $("textarea").each(function(){
-        var textareaidEL = $(this).attr("id");
-    
-        if ( textareaidEL < currentTime) {
-            $('textarea').addClass("past")
-            .removeClass("present")
-            .removeClass("future");
-
-        } else if ( textareaidEL = currentTime) {
-            $('textarea').addClass("present")
-            .removeClass("past")
-            .removeClass("future");
-
-        } else if ( textareaidEL > currentTime) {
-            $('textarea').addClass("future")
-            .removeClass("present")
-            .removeClass("past");
-        }
-    })
-    }
 
     // Data is retrieved from localStorage even after refreshing the page 
     $("#9am .description").val(localStorage.getItem("9am"));
@@ -48,6 +64,6 @@ $(document).ready(function () {
     $("#4pm .description").val(localStorage.getItem("4pm"));
     $("#5pm .description").val(localStorage.getItem("5pm"));
 
-displayTimeColor();
+    // displayTimeColor();
 
-})
+});
